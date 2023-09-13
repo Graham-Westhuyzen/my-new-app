9de5409e830bb1ff69412ffe7f19a78570b5d10e
Graham Westhuyzen <graham.westhuyzen@gmail.com>
2023-09-13 11:42:21 -0700

    commit first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar title='TitleOfWebsite' profilePhoto='./Images/pfp.jpeg' />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FDBA93',
+      },
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#F2AB91',
+
+          secondary: '#C2F2ED',
+
+          accent: '#FF8E79',
+
+          neutral: '#002743',
+
+          'base-100': '#10253E',
+
+          info: '#96AEFE',
+
+          success: '#51C083',
+
+          warning: '#FFC53E',
+
+          error: '#D94141',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
